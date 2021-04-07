8928181dcc290fa0feb2367e071c0062e6bc557e
felixgims <felixgims@outlook.com>
2021-04-07 18:23:58 +0200

    cambios index

--- final/src/main/resources/static/JS/Enviar.js
+++ final/src/main/resources/static/JS/Enviar.js
@@ -1,9 +1,9 @@
 function Enviar(){
 
-    const User = {
+    const usuario = {
         "username" : document.getElementById("username").value,
         "passwd"   : document.getElementById("password").value
     }
 
-    console.log(User)
+    console.log(usuario)
 }
