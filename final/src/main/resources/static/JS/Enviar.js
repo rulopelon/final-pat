cd236c69902868c5cdf05ae57608d12ddfcd6a63
felixgims <201708084@alu.comillas.edu>
2021-04-07 13:51:44 +0200

    cambios pequeños en index

---
+++ final/src/main/resources/static/JS/Enviar.js
@@ -0,0 +1,9 @@
+function Enviar(){
+
+    const User = {
+        "username" : document.getElementById("username").value,
+        "passwd"   : document.getElementById("password").value
+    }
+
+    console.log(User)
+}
