37c45c617a945c439d02d74105235f7c6bb636bd
felixgims <201708084@alu.comillas.edu>
2021-05-27 12:36:53 +0200

    cambio en cargar opciones

--- final/src/main/resources/static/JS/CargarOpciones.js
+++ final/src/main/resources/static/JS/CargarOpciones.js
@@ -1,4 +1,38 @@
 var x = document.getElementById("escoger");
-var option = document.createElement("option");
-option.text = "prueba";
 x.add(option);
+
+
+	let mensaje = {
+		id: localStorage.getItem("id")
+	}
+    const url = "/usuarios/getProfesores"
+	let mensajeEnviar = JSON.stringify(mensaje)
+	// se realiza la llamada al servidor
+	fetch(url,{
+		method:'GET',
+		headers:{
+		     'Content-Type': 'application/json; chartset=UTF-8'
+		},
+		body: mensajeEnviar
+	})
+	.then(r=>{
+		if(r.status ==200){
+			return r.json()
+
+		}else if(r.status==400){
+            alert("El usuario o la contraseña no cumplen los requisitos")
+        }else{
+			alert("Ha ocurrido un error al enviar el usuario y contraseña")
+		}
+	})
+	.then(respuesta=>{
+        console.log(respuesta)
+	}) 
+	.catch(e=>{
+		alert("Error de conexion")
+        console.log(e);
+	})
+	
+	if(localStorage.getItem(rol)=="alumno"){
+		window.location.href = "\reservas.html";
+	}
