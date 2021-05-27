5a5863f5a89ca8aa35ccd13dcd4fa5cd0268bf51
felixgims <201708084@alu.comillas.edu>
2021-05-27 12:55:36 +0200

    cambio js

--- final/src/main/resources/static/JS/home.js
+++ final/src/main/resources/static/JS/home.js
@@ -3,6 +3,5 @@ var rol = localStorage.getItem("rol");
 
 document.getElementById("Cola").style.display="none"
 if(rol=="profesor"){
-    console.log("hola")
     document.getElementById("Reservas").style.display="none"
 }
